375a80cfb46d43998fd26f86f26270c5f8dfedb6
manucabral <83357673+manucabral@users.noreply.github.com>
2021-11-20 17:00:29 -0300

    Update main.c

--- Brinf/main.c
+++ Brinf/main.c
@@ -9,14 +9,20 @@ int main(void)
         do
         {
             system("cls");
+            printf("\n\t%s v%s\n\t  by %s\n\n", APPNAME, VERSION, AUTHOR);
             DisplayBroswerInfo(Browser);
+            Notification("Info", "Select a option");
             DisplayOptions();
             option = GetOption();
 
+            system("cls");
             if(option == 1)
                 GetMostVisitedPages(Browser);
+            if(option == 2)
+                GetLastestVisitedPages(Browser);
+                
+            puts(" ");
             system("PAUSE");
-          
         } while (option > 0 && option < 5);
     }
     return 0;
